316a1a35002a66e0b76a14b57e245dd94cf3e730
miloskostadinovic <milos.kostadinovic@algometric.net>
2023-08-14 11:37:36 +0200

    fix

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
 export const API_URL = 'http://127.0.0.1:1337/api';
 // process.env.NEXT_PUBLIC_API_URL ||
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://127.0.0.1:3000';
