f1137418a6680846d747e92a677ffe4bf328f7c9
miloskostadinovic <milos.kostadinovic@algometric.net>
2023-08-16 11:50:57 +0200

    email address

--- pages/api/send.js
+++ pages/api/send.js
@@ -18,7 +18,7 @@ export default async (req, res) => {
   }
   try {
     const data = await resend.emails.send({
-      from: 'Portfolio <milos@miloskostadinovic.com>',
+      from: '<portfolio@miloskostadinovic.com>',
       to: [email],
       subject: 'Contact form miloskostadinovic.com',
       react: `Successfuly sent message.\n name:${name};\n email:${email};\n message:${message};\n`,
